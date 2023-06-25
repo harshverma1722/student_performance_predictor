e96bfaf1c82e4356b292c75a62d652e30888b8f8
harshverma1722 <vermaharsh.1722@gmail.com>
2023-06-25 12:51:46 +0530

    Added Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 from src.exception import CustomException
 
@@ -20,15 +22,19 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
-            # Train model
-            model.fit(X_train,y_train)
+            para = params[list(models.keys())[i]]
 
-            
+            gs = GridSearchCV(model,para,cv=3)
+            # Train model
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            #model.fit(X_train,y_train) 
             y_train_pred = model.predict(X_train)
             # Predict Testing data
             y_test_pred =model.predict(X_test)
