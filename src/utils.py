658f39fe5670bda64ee27fe79384c281ba114a97
harshverma1722 <vermaharsh.1722@gmail.com>
2023-06-25 15:41:51 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.logger import logging
-
+import pickle
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -52,3 +52,10 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,params):
             logging.info('Exception occured during model training')
             raise CustomException(e,sys)                                
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
